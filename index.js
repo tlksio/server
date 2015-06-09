031a635de5d7c5b230c2554ac1e007859d7d91a8
Raül Pérez <repejota@gmail.com>
2015-06-08 20:18:43 -0400

    log invalid hosts

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ var httpProxy = require('http-proxy');
 
 var proxy = httpProxy.createProxy();
 require('http').createServer(function(req, res) {
-    var url = "http://thereisnothinghere.io";
-
     var host = req.headers.host;
+    var url;
 
-    if (host==="tlks.io") {
+    if ((host==="tlks.io") || (host==="www.tlks.io")) {
         url = 'http://localhost:9001';
-    } else {
+    } else if (host==="api.tlks.io") {
         url = 'http://localhost:9002';
+    } else {
+        console.log(host);
     }
     proxy.web(req, res, {
         target: url

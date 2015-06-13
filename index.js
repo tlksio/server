c56292b3966847e5baa4a8f6c90e04e0b8dcf311
Raül Pérez <raul.perez@r3labs.io>
2015-06-13 02:50:55 +0200

    Add logs

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
+var fs = require('fs');
+var FileStreamRotator = require('file-stream-rotator');
 var http = require('http');
+var finalhandler = require('finalhandler');
+var morgan = require('morgan');
 var httpProxy = require('http-proxy');
 
-var proxy = httpProxy.createProxy();
-require('http').createServer(function(req, res) {
-    var host = req.headers.host;
-    var url;
+var accessLogStream = FileStreamRotator.getStream({
+    filename: __dirname + '/log/access.log',
+    frequency: 'daily',
+    verbose: false
+});
+var logger = morgan('combined', {
+    stream: accessLogStream
+});
 
-    if ((host === "tlks.io") ||  (host === "www.tlks.io")) {
-        url = 'http://localhost:9001';
-    } else if (host === "api.tlks.io") {
-        url = 'http://localhost:9002';
-    } else {
-        console.log(host);
-    }
-    proxy.web(req, res, {
-        target: url
+var proxy = httpProxy.createProxy();
+
+http.createServer(function(req, res) {
+    var done = finalhandler(req, res);
+    logger(req, res, function(err) {
+        var host = req.headers.host;
+        var url;
+        switch (host) {
+            case "tlks.io":
+            case "www.tlks.io":
+                url = 'http://localhost:9001';
+                proxy.web(req, res, {
+                    target: url
+                });
+                break;
+            case "api.tlks.io":
+                url = 'http://localhost:9002';
+                proxy.web(req, res, {
+                    target: url
+                });
+                break;
+            default:
+                console.log("ERROR: not allowed host", host);
+                break;
+        }
     });
 }).listen(80);
